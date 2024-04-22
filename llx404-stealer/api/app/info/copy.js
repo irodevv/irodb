52c96be4f309329df99575f4faf59cc9eaba0554
Zezzz <107312482+zougataga@users.noreply.github.com>
2024-04-22 05:09:18 +0200

    Delete llx404-stealer directory

--- llx404-stealer/api/app/info/copy.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    path: "/copy/:text",
-    method: "get",
-    go: async (req, res) => {
-        const pp = req.params.text;
-        if (!pp) return res.redirect("/");
-        return res.render("index.ejs", { content: pp })
-    }
-}
--- llx404-stealer/api/assets/function/web/ipinfo.js
+++
@@ -1,6 +0,0 @@
-const axios = require("axios");
-const ipinfo = async (ip) => {
-    const info = (await axios.get(`http://ip-api.com/json/${ip}`)).data;
-    return info;
-};
-module.exports = ipinfo;
